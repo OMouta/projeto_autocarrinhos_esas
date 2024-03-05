5691f107e4000bbc5865d05102782f748186e718
OMouta <tiagomouta609@gmail.com>
2024-03-05 23:07:03 +0000

    carro

--- main.cpp
+++ main.cpp
@@ -12,6 +12,23 @@ int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
+    cout << "                                                       AAAAAAAAAAAAAAAAAA                                            " << endl;
+    cout << "                                              AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA                               " << endl;
+    cout << "                                        AAAAAAAAAAAAAAA          AA      AAAAAAAAAAAAAAAAAAAA                        " << endl;
+    cout << "                                   AAAAAAAAAAA                   AA       AAAAAAAAAAAAAAAAAAAAAA                     " << endl;
+    cout << "                               AAAAAAAAA                        AA         AAAAAAAAAAAAAAAAAAAAAAAAAAAAA             " << endl;
+    cout << "                           AAAAAAAAA                       AAAAAAAAAAAAAAAAA AAAAAAAAAAAAAAAAAAAAAAAAAAAA            " << endl;
+    cout << "                 AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA        A           " << endl;
+    cout << "           AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA     A            " << endl;
+    cout << "       AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA         AAAAAAAAAAAAAAAAAAA           " << endl;
+    cout << "     A     AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA   AAAAAAA   AAAAAAAAAAAAAAAAA           " << endl;
+    cout << "   A      AAAAAAA            AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA   AAA     AAA   AAAAAAAAAAAAAA            " << endl;
+    cout << "  AAAAAAAANAAAAA  AAAAAAAAAA  AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA  AA         AAA  AAAAAAAAAAAA             " << endl;
+    cout << "  AAAAAAAAAAAAA  AAA      AAA  AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA  AAA          AA  AAAAAAAAAAA              " << endl;
+    cout << "  AAAAAAAAAAAA  AAA        AAA  AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA  AAA         AAA  AAAAAAAA                 " << endl;
+    cout << "  AAAAAAAAAAAA  AAA        AAA  AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA   AAA       AAA  AAA                       " << endl;
+    cout << "  AAAAAAAAAAAA   AA        AA   AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA    AAAAAAAAAAA                             " << endl;
+    cout << "   AAAAAAAAAAAA   AAAAAAAAAA   AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA                                          " << endl;
 
     return 0;
 }
